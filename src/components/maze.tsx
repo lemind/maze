8784a3598ec43236fa93ee428339e8e082014ec1
Dimitry Lyomin <leminds@gmail.com>
2021-08-11 08:43:56 +0300

    Add maze scheme proto

--- src/components/maze.tsx
+++ src/components/maze.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import mazeGenerator from 'generate-maze'
+import './maze.css'
+
+type TMazeItem = any
+type TProps = {
+  mazeItem: TMazeItem,
+}
+
+function MazeItem({mazeItem}: TProps) {
+  return <span className="Item">{mazeItem.x}_{mazeItem.y}</span>
+}
 
 export default function Maze() {
+  const maze = mazeGenerator(5)
+
+  console.log(maze)
+
   return (
     <div>
       <h3>maze</h3>
+
+      {maze.map(line => {
+        return <div className="Line">{
+          line.map((el: any) => {
+            return <MazeItem mazeItem={el} />
+          })
+        }</div>
+      })}
     </div>
   )
 }
